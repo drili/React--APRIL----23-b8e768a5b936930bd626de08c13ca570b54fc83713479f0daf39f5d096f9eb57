8eaa98ab38cdd57c65f97e4a8e202451c5f7dd8b
drili <DriliB@hotmail.com>
2023-04-09 20:27:43 +0200

    :hammer: Recipes model and routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import mongoose from "mongoose"
 
 import { userRouter } from "./routes/users.js"
+import { recipesRouter } from "./routes/recipes.js"
 
 const app = express()
 const PORT = 3001
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/auth", userRouter)
+app.use("/recipes", recipesRouter)
 
 mongoose.connect(
     "mongodb+srv://dbkynetic:Kynetic123123@cluster0.f80a2n8.mongodb.net/?retryWrites=true&w=majority"
